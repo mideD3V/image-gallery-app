86b12517bd83c9479fd69fea1e0c3276cd37b9b2
mided3v <ayo4real6@gmail.com>
2023-09-21 15:33:29 +0100

    added icon to the search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import { useState } from 'react'
 
+import { FiSearch } from "react-icons/fi";
+
 const Search = () => {
 
     const [searchTerm, setSearchTerm] = useState('')
 
     
   return (
-      <div id='searchbar'>
-          <input type="search" name="searchbar" id="search" placeholder='Search images...' onChange={(e)=>{setSearchTerm(e.target.value)}}/>
+    <div id="searchbar">
+      <input
+        type="search"
+        name="searchbar"
+        id="search"
+        placeholder="Search images..."
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+        }}
+      />{" "}
+      <FiSearch className="icon" />
     </div>
-  )
+  );
 }
 
 export default Search
